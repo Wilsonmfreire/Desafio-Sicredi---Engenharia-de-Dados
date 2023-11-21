f3757531e5cd89765069164d30b3a1790a08c1dd
Wilson Morais Freire <62375886+Wilsonmfreire@users.noreply.github.com>
2023-11-21 10:31:36 -0300

    subindo projeto
    estrutura de pastas (initdb e jars).
    arquivos utilizados para o desenvolvimento.

---
+++ initdb/03_insert.sql
@@ -0,0 +1,120 @@
+INSERT INTO SICREDI.associado (nome, sobrenome, idade, email) VALUES
+  ('João', 'Silva', 30, 'joao.silva@example.com'),
+  ('Maria', 'Santos', 25, 'maria.santos@example.com'),
+  ('Pedro', 'Souza', 40, 'pedro.souza@example.com'),
+  ('Ana', 'Clara', 35, 'ana.clara@example.com'),
+  ('José', 'Fernandes', 20, 'jose.fernandes@example.com'),
+  ('Clara', 'Martins', 15, 'clara.martins@example.com'),
+  ('Luís', 'Alves', 50, 'luis.alves@example.com'),
+  ('Fernanda', 'Pereira', 60, 'fernanda.pereira@example.com'),
+  ('Carlos', 'Oliveira', 70, 'carlos.oliveira@example.com');
+INSERT INTO SICREDI.associado (nome, sobrenome, idade, email) VALUES
+  ('Ana Paula', 'Marques', 28, 'ana.paula.marques@example.com'),
+  ('Carlos Alberto', 'Cunha', 32, 'carlos.alberto.cunha@example.com'),
+  ('Daniela', 'Souza', 37, 'daniela.souza@example.com'),
+  ('Eduardo', 'Santos', 42, 'eduardo.santos@example.com'),
+  ('Fernanda', 'Pereira', 47, 'fernanda.pereira@example.com'),
+  ('Gabriel', 'Silva', 52, 'gabriel.silva@example.com'),
+  ('Henrique', 'Martins', 57, 'henrique.martins@example.com'),
+  ('Isabela', 'Alves', 62, 'isabela.alves@example.com'),
+  ('João', 'Fernandes', 67, 'joao.fernandes@example.com');
+INSERT INTO SICREDI.associado (nome, sobrenome, idade, email) VALUES
+  ('Maria Clara', 'Ferreira', 23, 'maria.clara.ferreira@example.com'),
+  ('José Carlos', 'Santos', 28, 'jose.carlos.santos@example.com'),
+  ('Luísa', 'Pereira', 33, 'luisa.pereira@example.com'),
+  ('Pedro Henrique', 'Silva', 38, 'pedro.henrique.silva@example.com'),
+  ('Ana Beatriz', 'Alves', 43, 'ana.beatriz.alves@example.com'),
+  ('Carlos Eduardo', 'Martins', 48, 'carlos.eduardo.martins@example.com'),
+  ('Daniel', 'Fernandes', 53, 'daniel.fernandes@example.com'),
+  ('Eduarda', 'Silva', 58, 'eduarda.silva@example.com'),
+  ('João Paulo', 'Ferreira', 63, 'joao.paulo.ferreira@example.com');
+INSERT INTO SICREDI.associado (nome, sobrenome, idade, email) VALUES
+  ('Mariana', 'Cunha', 24, 'mariana.cunha@example.com'),
+  ('João Pedro', 'Santos', 29, 'joao.pedro.santos@example.com'),
+  ('Luísa Fernanda', 'Pereira', 34, 'luisa.fernanda.pereira@example.com'),
+  ('Pedro Paulo', 'Silva', 39, 'pedro.paulo.silva@example.com'),
+  ('Ana Carolina', 'Alves', 44, 'ana.carolina.alves@example.com'),
+  ('Carlos Fernando', 'Martins', 49, 'carlos.fernando.martins@example.com'),
+  ('Daniela', 'Fernandes', 54, 'daniela.fernandes@example.com'),
+  ('Eduardo', 'Silva', 59, 'eduardo.silva@example.com'),
+  ('João Vitor', 'Ferreira', 64, 'joao.vitor.ferreira@example.com');
+INSERT INTO SICREDI.associado (nome, sobrenome, idade, email) VALUES
+  ('Gabriela', 'Martins', 25, 'gabriela.martins@example.com'),
+  ('João Lucas', 'Santos', 30, 'joao.lucas.santos@example.com'),
+  ('Luísa Gabriela', 'Pereira', 35, 'luisa.gabriela.pereira@example.com'),
+  ('Pedro Henrique', 'Silva', 40, 'pedro.henrique.silva@example.com'),
+  ('Ana Beatriz', 'Alves', 45, 'ana.beatriz.alves@example.com'),
+  ('Carlos Eduardo', 'Martins', 50, 'carlos.eduardo.martins@example.com'),
+  ('Daniel', 'Fernandes', 55, 'daniel.fernandes@example.com'),
+  ('Eduarda', 'Silva', 60, 'eduarda.silva@example.com'),
+  ('João Paulo', 'Ferreira', 65, 'joao.paulo.ferreira@example.com');
+INSERT INTO SICREDI.associado (nome, sobrenome, idade, email) VALUES
+  ('Heitor', 'Cunha', 26, 'heitor.cunha@example.com'),
+  ('João Pedro', 'Santos', 31, 'joao.pedro.santos@example.com'),
+  ('Luísa Fernanda', 'Pereira', 36, 'luisa.fernanda.pereira@example.com'),
+  ('Pedro Paulo', 'Silva', 41, 'pedro.paulo.silva@example.com'),
+  ('Ana Carolina', 'Alves', 46, 'ana.carolina.alves@example.com'),
+  ('Carlos Fernando', 'Martins', 51, 'carlos.fernando.martins@example.com'),
+  ('Daniela', 'Fernandes', 56, 'daniela.fernandes@example.com'),
+  ('Eduardo', 'Silva', 61, 'eduardo.silva@example.com'),
+  ('João Vitor', 'Ferreira', 66, 'joao.vitor.ferreira@example.com');
+INSERT INTO SICREDI.associado (nome, sobrenome, idade, email) VALUES
+  ('Igor', 'Martins', 27, 'igor.martins@example.com'),
+  ('João Lucas', 'Santos', 32, 'joao.lucas.santos@example.com'),
+  ('Luísa Gabriela', 'Pereira', 37, 'luisa.gabriela.pereira@example.com'),
+  ('Pedro Henrique', 'Silva', 42, 'pedro.henrique.silva@example.com'),
+  ('Ana Beatriz', 'Alves', 47, 'ana.beatriz.alves@example.com'),
+  ('Carlos Eduardo', 'Martins', 52, 'carlos.eduardo.martins@example.com'),
+  ('Daniel', 'Fernandes', 57, 'daniel.fernandes@example.com'),
+  ('Eduarda', 'Silva', 62, 'eduarda.silva@example.com'),
+  ('João Paulo', 'Ferreira', 67, 'joao.paulo.ferreira@example.com');
+INSERT INTO SICREDI.associado (nome, sobrenome, idade, email) VALUES
+  ('Lucas', 'Cunha', 28, 'lucas.cunha@example.com'),
+  ('João Pedro', 'Santos', 33, 'joao.pedro.santos@example.com'),
+  ('Luísa Fernanda', 'Pereira', 38, 'luisa.fernanda.pereira@example.com'),
+  ('Pedro Henrique', 'Silva', 43, 'pedro.henrique.silva@example.com'),
+  ('Ana Beatriz', 'Alves', 48, 'ana.beatriz.alves@example.com'),
+  ('Carlos Eduardo', 'Martins', 53, 'carlos.eduardo.martins@example.com'),
+  ('Daniel', 'Fernandes', 58, 'daniel.fernandes@example.com'),
+  ('Eduarda', 'Silva', 63, 'eduarda.silva@example.com'),
+  ('João Paulo', 'Ferreira', 68, 'joao.paulo.ferreira@example.com');
+INSERT INTO SICREDI.associado (nome, sobrenome, idade, email) VALUES
+  ('Miguel', 'Martins', 29, 'miguel.martins@example.com'),
+  ('João Lucas', 'Santos', 34, 'joao.lucas.santos@example.com'),
+  ('Luísa Gabriela', 'Pereira', 39, 'luisa.gabriela.pereira@example.com'),
+  ('Pedro Henrique', 'Silva', 44, 'pedro.henrique.silva@example.com'),
+  ('Ana Beatriz', 'Alves', 49, 'ana.beatriz.alves@example.com'),
+  ('Carlos Eduardo', 'Martins', 54, 'carlos.eduardo.martins@example.com'),
+  ('Daniel', 'Fernandes', 59, 'daniel.fernandes@example.com'),
+  ('Eduarda', 'Silva', 64, 'eduarda.silva@example.com'),
+  ('João Paulo', 'Ferreira', 69, 'joao.paulo.ferreira@example.com');
+INSERT INTO SICREDI.associado (nome, sobrenome, idade, email) VALUES
+  ('Rafael', 'Cunha', 30, 'rafael.cunha@example.com'),
+  ('João Pedro', 'Santos', 35, 'joao.pedro.santos@example.com'),
+  ('Luísa Fernanda', 'Pereira', 40, 'luisa.gabriela.pereira@example.com'),
+  ('Pedro Henrique', 'Silva', 45, 'pedro.henrique.silva@example.com'),
+  ('Ana Beatriz', 'Alves', 50, 'ana.beatriz.alves@example.com'),
+  ('Carlos Eduardo', 'Martins', 55, 'carlos.eduardo.martins@example.com'),
+  ('Daniel', 'Fernandes', 60, 'daniel.fernandes@example.com'),
+  ('Eduarda', 'Silva', 65, 'eduarda.silva@example.com'),
+  ('João Paulo', 'Ferreira', 70, 'joao.paulo.ferreira@example.com');
+INSERT INTO SICREDI.associado (nome, sobrenome, idade, email) VALUES
+  ('Thiago', 'Martins', 31, 'thiago.martins@example.com'),
+  ('João Pedro', 'Santos', 36, 'joao.pedro.santos@example.com'),
+  ('Luísa Gabriela', 'Pereira', 41, 'luisa.gabriela.pereira@example.com'),
+  ('Pedro Henrique', 'Silva', 46, 'pedro.henrique.silva@example.com'),
+  ('Ana Beatriz', 'Alves', 51, 'ana.beatriz.alves@example.com'),
+  ('Carlos Eduardo', 'Martins', 56, 'carlos.eduardo.martins@example.com'),
+  ('Daniel', 'Fernandes', 61, 'daniel.fernandes@example.com'),
+  ('Eduarda', 'Silva', 66, 'eduarda.silva@example.com'),
+  ('João Paulo', 'Ferreira', 71, 'joao.paulo.ferreira@example.com');
+INSERT INTO SICREDI.associado (nome, sobrenome, idade, email) VALUES
+  ('Vitor', 'Cunha', 32, 'vitor.cunha@example.com'),
+  ('João Pedro', 'Santos', 37, 'joao.pedro.santos@example.com'),
+  ('Luísa Gabriela', 'Pereira', 42, 'luisa.gabriela.pereira@example.com'),
+  ('Pedro Henrique', 'Silva', 47, 'pedro.henrique.silva@example.com'),
+  ('Ana Beatriz', 'Alves', 52, 'ana.beatriz.alves@example.com'),
+  ('Carlos Eduardo', 'Martins', 57, 'carlos.eduardo.martins@example.com'),
+  ('Daniel', 'Fernandes', 62, 'daniel.fernandes@example.com'),
+  ('Eduarda', 'Silva', 67, 'eduarda.silva@example.com'),
+  ('João Paulo', 'Ferreira', 72, 'joao.paulo.ferreira@example.com');
